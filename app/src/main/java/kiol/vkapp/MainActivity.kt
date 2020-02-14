7875591f02f6241587d9e44d4b88b7a64848bf8a
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-15 02:49:41 +0300

    Initial TaskA

--- app/src/main/java/kiol/vkapp/MainActivity.kt
+++ app/src/main/java/kiol/vkapp/MainActivity.kt
@@ -2,70 +2,177 @@ package kiol.vkapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegate
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthCallback
-import io.reactivex.Flowable
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.schedulers.Timed
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.exceptions.VKApiExecutionException
+import com.vk.api.sdk.requests.VKRequest
+import org.json.JSONObject
 import timber.log.Timber
+import java.util.*
+import kotlin.math.abs
 
-data class SampleData(val userId: Int, val title: String)
 
-interface SampleRetrofit {
-    @GET("posts")
-    fun getSample(): Flowable<List<SampleData>>
-}
+data class DocItem(val docTitle: String, val docInfo: String, val tags: String, val type: Int, val imageTemp: String)
+
+data class VKDocItem(
+    val title: String, val size: Long, val ext: String, val date: Long, val tags: List<String>?, val type: Int,
+    val preview: VKPhotoPreview?
+)
+
+data class VKResponse<T>(val response: T)
+
+data class VKListContainerResponse<T>(val count: Int, val items: List<T>)
+
+data class VKImagePreview(val src: String, val width: Int, val height: Int)
+
+data class VKPhotoPreview(val photo: VKSizesPreview?)
+
+data class VKSizesPreview(val sizes: List<VKImagePreview>)
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ListDelegationAdapter<List<DocItem>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
+        if (VK.isLoggedIn()) {
+            getDocs()
+        } else {
+            VK.login(this, arrayListOf(VKScope.DOCS))
+        }
 
-        val d = retrofit.create(SampleRetrofit::class.java).getSample().subscribeOn(Schedulers.io()).observeOn(
-            AndroidSchedulers
-                .mainThread()
-        ).subscribe({
-            Timber.d("Test: $it")
-        }, {
-            Timber.e("Error: $it")
+        val docs = findViewById<RecyclerView>(R.id.docs)
+
+        adapter = ListDelegationAdapter<List<DocItem>>(adapterDelegate(R.layout.doc_item_layout) {
+            val titleTv = findViewById<TextView>(R.id.docTitleTv)
+            val imageTv = findViewById<ImageView>(R.id.docImageIv)
+            val infoTv = findViewById<TextView>(R.id.docInfoTv)
+            val tagsTv = findViewById<TextView>(R.id.docTagsTv)
+            bind {
+                if (item.imageTemp.isNotEmpty()) {
+                    imageTv.load(item.imageTemp)
+                }
+                titleTv.text = item.docTitle
+                infoTv.text = item.docInfo
+                if (item.tags.isEmpty()) {
+                    tagsTv.visibility = View.GONE
+                } else {
+                    tagsTv.text = item.tags
+                    tagsTv.visibility = View.VISIBLE
+                }
+            }
         })
-
-
-        findViewById<ImageView>(R.id.image).load("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
-
-        //        Timber.d("Test log")
-        //VK.login(this, arrayListOf(VKScope.WALL))
+        docs.adapter = adapter
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val callback = object : VKAuthCallback {
             override fun onLogin(token: VKAccessToken) {
-                // User passed authorization
+                getDocs()
             }
 
             override fun onLoginFailed(errorCode: Int) {
-                // User didn't pass authorization
+                Timber.e("onLoginFailed $errorCode")
             }
         }
         if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    private fun getDocs() {
+        val request = VKRequest<JSONObject>("docs.get").addParam("return_tags", 1)
+
+        VK.execute(request, object : VKApiCallback<JSONObject> {
+            override fun fail(error: VKApiExecutionException) {
+                Timber.e("getDocs failed $error")
+            }
+
+            override fun success(result: JSONObject) {
+                Timber.d("getDocs success $result")
+
+                val docsResponse: VKResponse<VKListContainerResponse<VKDocItem>> = Gson().fromJson(result.toString())
+                adapter.items = docsResponse.response.items.map {
+                    DocItem(
+                        it.title,
+                        "${it.ext
+                            .toUpperCase()} · ${humanReadableByteCountSI(it.size)} · ${humanReadableTimestamp(it.date * 1000)}",
+                        it.tags
+                            ?.joinToString(", ") ?: "",
+                        0,
+                        it.preview?.photo
+                            ?.sizes?.firstOrNull()?.src ?: ""
+                    )
+                }
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object : TypeToken<T>() {}.type)
+
+    fun humanReadableTimestamp(timestamp: Long): String {
+        val nowTime: Calendar = Calendar.getInstance()
+        val neededTime: Calendar = Calendar.getInstance()
+        neededTime.timeInMillis = timestamp
+
+        return if (neededTime.get(Calendar.YEAR) == nowTime.get(Calendar.YEAR)) {
+            when {
+                nowTime.get(Calendar.DATE) == neededTime.get(Calendar.DATE) -> {
+                    "Сегодня"
+                }
+                nowTime.get(Calendar.DATE) - neededTime.get(Calendar.DATE) == 1 -> {
+                    "Вчера"
+                }
+                else -> {
+                    DateFormat.format("dd MMMM", neededTime).toString()
+                }
+            }
+        } else {
+            DateFormat.format("yyyy dd MMMM", neededTime).toString()
+        }
+    }
+
+    fun humanReadableByteCountSI(bytes: Long): String? {
+        val s = if (bytes < 0) "-" else ""
+        var b =
+            if (bytes == Long.MIN_VALUE) Long.MAX_VALUE else abs(bytes)
+        return if (b < 1000L) "$bytes Б" else if (b < 999950L) String.format(
+            "%s%.1f КБ",
+            s,
+            b / 1e3
+        ) else if (1000.let { b /= it; b } < 999950L) String.format(
+            "%s%.1f МБ",
+            s,
+            b / 1e3
+        ) else if (1000.let { b /= it; b } < 999950L) String.format(
+            "%s%.1f ГБ",
+            s,
+            b / 1e3
+        ) else if (1000.let { b /= it; b } < 999950L) String.format(
+            "%s%.1f ТБ",
+            s,
+            b / 1e3
+        ) else if (1000.let { b /= it; b } < 999950L) String.format(
+            "%s%.1f ПБ",
+            s,
+            b / 1e3
+        ) else String.format("%s%.1f ЭБ", s, b / 1e6)
     }
 }
