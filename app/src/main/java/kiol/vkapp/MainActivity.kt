f9304bf0d147a88db9cdbd3fd94ac766ea208ee6
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-14 15:38:33 +0300

    Initial commit

---
+++ app/src/main/java/kiol/vkapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package kiol.vkapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
