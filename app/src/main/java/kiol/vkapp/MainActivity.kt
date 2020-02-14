02e434c2ce701b3fe7e45fafdeae3ad93ad261b7
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-14 16:12:04 +0300

    Initial VK sdk

--- app/src/main/java/kiol/vkapp/MainActivity.kt
+++ app/src/main/java/kiol/vkapp/MainActivity.kt
@@ -1,12 +1,35 @@
 package kiol.vkapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        VK.login(this, arrayListOf(VKScope.WALL))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object : VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                // User passed authorization
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
