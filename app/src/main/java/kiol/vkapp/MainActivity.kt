8ae7acf941e5f726b33cf22d6cc65dcd1db19851
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-15 10:28:20 +0300

    Remove and rename actions

--- app/src/main/java/kiol/vkapp/MainActivity.kt
+++ app/src/main/java/kiol/vkapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package kiol.vkapp
 
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.text.format.DateFormat
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.view.doOnLayout
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.google.gson.Gson
@@ -26,9 +34,15 @@ import java.util.*
 import kotlin.math.abs
 
 
-data class DocItem(val docTitle: String, val docInfo: String, val tags: String, val type: Int, val imageTemp: String)
+data class DocItem(
+    val id: Int,
+    val owner_id: Int, val docTitle: String, val docInfo: String, val tags: String, val type: Int, val images:
+    VKSizesPreview?
+)
 
 data class VKDocItem(
+    val id: Int,
+    val owner_id: Int,
     val title: String, val size: Long, val ext: String, val date: Long, val tags: List<String>?, val type: Int,
     val preview: VKPhotoPreview?
 )
@@ -42,6 +56,32 @@ data class VKImagePreview(val src: String, val width: Int, val height: Int)
 data class VKPhotoPreview(val photo: VKSizesPreview?)
 
 data class VKSizesPreview(val sizes: List<VKImagePreview>)
+
+fun ImageView.loadVKImages(vkPreviews: VKSizesPreview?) {
+    doOnLayout {
+        val tw = measuredWidth
+        val th = measuredHeight
+
+        var best = vkPreviews?.sizes?.firstOrNull()
+        vkPreviews?.sizes?.forEach {
+            val ib = best
+            if (ib == null) {
+                best = it
+            } else {
+                if (it.width >= it.height) {
+                    if (it.width <= tw && it.width > ib.width) best = it
+                } else {
+                    if (it.height <= th && it.height > ib.height) best = it
+                }
+            }
+        }
+
+        best?.let {
+            load(it.src)
+        }
+    }
+}
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -60,13 +100,17 @@ class MainActivity : AppCompatActivity() {
         val docs = findViewById<RecyclerView>(R.id.docs)
 
         adapter = ListDelegationAdapter<List<DocItem>>(adapterDelegate(R.layout.doc_item_layout) {
+
             val titleTv = findViewById<TextView>(R.id.docTitleTv)
             val imageTv = findViewById<ImageView>(R.id.docImageIv)
             val infoTv = findViewById<TextView>(R.id.docInfoTv)
             val tagsTv = findViewById<TextView>(R.id.docTagsTv)
+            val menuBtn = findViewById<ImageButton>(R.id.docMenuBtn)
             bind {
-                if (item.imageTemp.isNotEmpty()) {
-                    imageTv.load(item.imageTemp)
+                imageTv.loadVKImages(item.images)
+
+                menuBtn.setOnClickListener {
+                    showPopup(it, item, adapter)
                 }
                 titleTv.text = item.docTitle
                 infoTv.text = item.docInfo
@@ -79,6 +123,47 @@ class MainActivity : AppCompatActivity() {
             }
         })
         docs.adapter = adapter
+    }
+
+    private fun showPopup(v: View, docItem: DocItem, adapter: ListDelegationAdapter<List<DocItem>>) {
+        PopupMenu(this, v).apply {
+            // MainActivity implements OnMenuItemClickListener
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.renameMenu -> {
+                        showRenameDialog(docItem, adapter)
+                    }
+                    R.id.removeMenu -> {
+                        removeDoc(docItem)
+                        val index = adapter.items.indexOf(docItem)
+                        (adapter.items as MutableList).removeAt(index)
+                        adapter.notifyItemRemoved(index)
+                    }
+                }
+                true
+            }
+            inflate(R.menu.doc_item_menu)
+            show()
+        }
+    }
+
+    private fun showRenameDialog(docItem: DocItem, adapter: ListDelegationAdapter<List<DocItem>>) {
+        val taskEditText = EditText(this)
+        taskEditText.setText(docItem.docTitle)
+        val dialog: AlertDialog = AlertDialog.Builder(this)
+            .setTitle("Название документа")
+            .setView(taskEditText)
+            .setPositiveButton("Save") { _, _ ->
+                val index = adapter.items.indexOf(docItem)
+                val newDocItem = docItem.copy(docTitle = taskEditText.text.toString())
+                (adapter.items as MutableList)[index] = newDocItem
+                adapter.notifyItemChanged(index)
+                updateDocName(newDocItem)
+
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+        dialog.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -110,6 +195,7 @@ class MainActivity : AppCompatActivity() {
                 val docsResponse: VKResponse<VKListContainerResponse<VKDocItem>> = Gson().fromJson(result.toString())
                 adapter.items = docsResponse.response.items.map {
                     DocItem(
+                        it.id, it.owner_id,
                         it.title,
                         "${it.ext
                             .toUpperCase()} · ${humanReadableByteCountSI(it.size)} · ${humanReadableTimestamp(it.date * 1000)}",
@@ -117,10 +203,43 @@ class MainActivity : AppCompatActivity() {
                             ?.joinToString(", ") ?: "",
                         0,
                         it.preview?.photo
-                            ?.sizes?.firstOrNull()?.src ?: ""
+
                     )
                 }
                 adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    private fun removeDoc(docItem: DocItem) {
+        val request = VKRequest<JSONObject>("docs.delete")
+            .addParam("owner_id", docItem.owner_id)
+            .addParam("doc_id", docItem.id)
+
+        VK.execute(request, object : VKApiCallback<JSONObject> {
+            override fun fail(error: VKApiExecutionException) {
+                Timber.e("removeDoc failed $error")
+            }
+
+            override fun success(result: JSONObject) {
+                Timber.d("removeDoc success $result")
+            }
+        })
+    }
+
+    private fun updateDocName(docItem: DocItem) {
+        val request = VKRequest<JSONObject>("docs.edit")
+            .addParam("owner_id", docItem.owner_id)
+            .addParam("doc_id", docItem.id)
+            .addParam("title", docItem.docTitle)
+
+        VK.execute(request, object : VKApiCallback<JSONObject> {
+            override fun fail(error: VKApiExecutionException) {
+                Timber.e("updateDocName failed $error")
+            }
+
+            override fun success(result: JSONObject) {
+                Timber.d("updateDocName success $result")
             }
         })
     }
