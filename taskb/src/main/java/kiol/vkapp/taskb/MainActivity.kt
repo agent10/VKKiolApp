e5c110434fbaf35d8a0c3535e09f510865bdb8fa
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-18 11:18:24 +0300

    TaskB template added

---
+++ taskb/src/main/java/kiol/vkapp/taskb/MainActivity.kt
@@ -0,0 +1,15 @@
+package kiol.vkapp.taskb
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
