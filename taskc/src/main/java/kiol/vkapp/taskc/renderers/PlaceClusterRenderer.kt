62d5a91268331a4852fab4530e78f9c583ee8eed
Kirill Olenyov <olenyov.kirill@me.com>
2020-03-01 00:07:51 +0300

    MinDistance

--- taskc/src/main/java/kiol/vkapp/taskc/renderers/PlaceClusterRenderer.kt
+++ taskc/src/main/java/kiol/vkapp/taskc/renderers/PlaceClusterRenderer.kt
@@ -1,9 +1,12 @@
 package kiol.vkapp.taskc.renderers
 
 import android.content.Context
+import android.location.Location
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.SphericalUtil
 import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import kiol.vkapp.commondata.domain.PlaceType
@@ -15,6 +18,11 @@ class PlaceClusterRenderer(
     ClusterManager<PlaceClusterItem>, private val markerImageGenerator: MarkerImageGenerator
 ) :
     CustomClusterRenderer<PlaceClusterItem>(context, googleMap, clusterManager) {
+
+    companion object {
+        private const val MinDistanceCluster = 4
+    }
+
     init {
         minClusterSize = 1
     }
@@ -61,4 +69,37 @@ class PlaceClusterRenderer(
     }
 
     override fun onClusterItemUpdated(item: PlaceClusterItem?, marker: Marker?) {}
+
+    override fun shouldRenderAsCluster(cluster: Cluster<PlaceClusterItem>?): Boolean {
+        val c1 = super.shouldRenderAsCluster(cluster)
+
+        var c2 = true
+        cluster?.run {
+            if (size in 2..4) {
+                var centerLat = 0.0
+                var centerLong = 0.0
+                items.forEach {
+                    centerLat += it.position.latitude
+                    centerLong += it.position.longitude
+                }
+                centerLat /= size
+                centerLong /= size
+                val centerCluser = LatLng(centerLat, centerLong)
+
+                var maxRadius = Double.MIN_VALUE
+                items.forEach {
+                    val d = SphericalUtil.computeDistanceBetween(centerCluser, it.position)
+                    if (d > maxRadius) {
+                        maxRadius = d
+                    }
+                }
+
+                if (maxRadius >= 0 && maxRadius < MinDistanceCluster) {
+                    c2 = false
+                }
+            }
+        }
+
+        return c1 && c2
+    }
 }
