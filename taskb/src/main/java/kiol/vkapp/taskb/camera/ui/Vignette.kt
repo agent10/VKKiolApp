2fc98e8fb2e236a9eaa071b28f2174835011c1de
Kirill Olenyov <olenyov.kirill@me.com>
2020-02-29 14:21:32 +0300

    Next

--- taskb/src/main/java/kiol/vkapp/taskb/camera/ui/Vignette.kt
+++ taskb/src/main/java/kiol/vkapp/taskb/camera/ui/Vignette.kt
@@ -9,24 +9,28 @@ import android.graphics.drawable.shapes.RectShape
 import android.util.AttributeSet
 import android.view.View
 
+class VignetteDrawable : ShapeDrawable(RectShape()) {
+    init {
+        shaderFactory = object : ShapeDrawable.ShaderFactory() {
+            override fun resize(width: Int, height: Int): Shader {
+                return LinearGradient(
+                    width / 2f,
+                    0f,
+                    width / 2f,
+                    height.toFloat(),
+                    intArrayOf(0x77000000, Color.TRANSPARENT, Color.TRANSPARENT, 0x88000000.toInt()),
+                    floatArrayOf(0f, 0.15f, 0.65f, 1f),
+                    Shader.TileMode.CLAMP
+                )
+            }
+        }
+    }
+}
+
 class Vignette @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     init {
-        background = ShapeDrawable(RectShape()).apply {
-            shaderFactory = object : ShapeDrawable.ShaderFactory() {
-                override fun resize(width: Int, height: Int): Shader {
-                    return LinearGradient(
-                        width / 2f,
-                        0f,
-                        width / 2f,
-                        height.toFloat(),
-                        intArrayOf(0x77000000.toInt(), Color.TRANSPARENT, Color.TRANSPARENT, 0x88000000.toInt()),
-                        floatArrayOf(0f, 0.15f, 0.65f, 1f),
-                        Shader.TileMode.CLAMP
-                    )
-                }
-            }
-        }
+        background = VignetteDrawable()
     }
 }
